87feda4278046d57b35b0897ed15896c74091a39
ngue <ngue.ntamak@gmail.com>
2017-12-25 07:11:44 +0100

    Four modifications functional_tests.py, home.html

--- functional_tests.py
+++ functional_tests.py
@@ -33,14 +33,14 @@ class NewVisitorTest(unittest.TestCase):
 	# when she hits enter, the page updated, and now the page list
 	# "1: Buy peacock feathers"as an item in to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(3)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(rows.text == '1: Buy peacock feathers' for row in rows),
-            'New to-do item did not appear to in table'
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows])
 
 
 	# There is still a textbox inviting her to add another item. She
